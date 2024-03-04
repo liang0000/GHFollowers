bce60bda426d04bb012964bd10d86398fdeab311
Liang0 <saurabh.karajgikar@dhl.com>
2024-03-04 17:46:45 +0800

    update

---
+++ GHFollowers/Custom Views/Cells/FavouriteCell.swift
@@ -0,0 +1,44 @@
+//
+
+import UIKit
+
+class FavouriteCell: UITableViewCell {
+    static let reuseID  = "FavouriteCell"
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel   = GFTitleLabel(textAlignment: .left, fontSize: 26)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(favourite: Follower) {
+        usernameLabel.text = favourite.login
+        avatarImageView.downloadImage(from: favourite.avatarUrl)
+    }
+    
+    private func configure() {
+		let padding: CGFloat = 12
+		
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        accessoryType = .disclosureIndicator // tappable and more to see
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            
+            usernameLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
+            usernameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+}
