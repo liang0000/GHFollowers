68c38148aaeaa78c41253e8748e5d7760027e74d
Liang0 <saurabh.karajgikar@dhl.com>
2024-03-09 18:20:04 +0800

    update

---
+++ GHFollowers/Extensions/UITableView+Ext.swift
@@ -0,0 +1,14 @@
+//
+
+
+import UIKit
+
+extension UITableView {
+    func reloadDataOnMainThread() {
+        DispatchQueue.main.async { self.reloadData() }
+    }
+
+    func removeExcessCells() {
+        tableFooterView = UIView(frame: .zero) // remove empty cell
+    }
+}
--- GHFollowers/Screens/FavouritesListVC.swift
+++ GHFollowers/Screens/FavouritesListVC.swift
@@ -31,6 +31,7 @@ class FavouritesListVC: UIViewController {
 		tableView.rowHeight     = 80
 		tableView.delegate      = self
 		tableView.dataSource    = self
+		tableView.removeExcessCells()
 		
 		tableView.register(FavouriteCell.self, forCellReuseIdentifier: FavouriteCell.reuseID)
 	}
@@ -81,13 +82,13 @@ extension FavouritesListVC: UITableViewDataSource, UITableViewDelegate {
 	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) { // to delete the row
 		guard editingStyle == .delete else { return } // if not doing delete then don't do anything
 		
-		let favourite = favourites[indexPath.row]
-		favourites.remove(at: indexPath.row)
-		tableView.deleteRows(at: [indexPath], with: .left)
-		
-		PersistenceManager.updateWith(favourite: favourite, actionType: .remove) { [weak self] error in
+		PersistenceManager.updateWith(favourite: favourites[indexPath.row], actionType: .remove) { [weak self] error in
 			guard let self = self else { return }
-			guard let error = error else { return }
+			guard let error = error else {
+				self.favourites.remove(at: indexPath.row)
+				tableView.deleteRows(at: [indexPath], with: .left)
+				return
+			}
 			self.presentGFAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
 		}
 	}
